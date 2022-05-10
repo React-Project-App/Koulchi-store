ca2cd830a3e13525d858272efc5772cd36f4bf7d
walid <walid@gmail.com>
2022-05-10 22:51:45 +0200

    Add Style to ProductDetails.jsx

--- src/Pages/ProductDetails/ProductDetails.jsx
+++ src/Pages/ProductDetails/ProductDetails.jsx
@@ -5,6 +5,7 @@ import { GETDETAILSPRODUCT } from "../../Actions/Poroducts";
 
 import { Link } from "react-router-dom";
 import { AddTocart } from "../../Actions/Cart";
+import "../../CssFiles/style.css"
 const ProductDetails = () => {
   const { Id } = useParams();
 
@@ -19,45 +20,51 @@ const ProductDetails = () => {
     setpic(e.currentTarget.src);
   };
   return Title  ? (
-    <div className="container">
-      <div className="card shadow h-75 col-10">
-        <div className="card-body">
-          <div className="w-100 border border-primary rounded rounded-5 d-flex justify-content-center">
-            <img
-              className="card-img-top w-50 "
-              width="300px"
-              src={pic ? pic : Photo}
-              alt={Title}
-            />
+    <main class="container">
+    <div class="row pt-5">
+      <div class="col-lg-5 col-sm-12 col-md-8 row pt-3">
+          <div class="col-12 big w-auto">
+          <img 
+            src={pic ? pic : Photo}
+            class="img-fluid" 
+            alt={Title}/>
           </div>
-          <dir className="my-4">
-            <h1 className="card-title d-inline text">{Title}</h1>
-            <h4 className="text-primary ps-4 d-inline">({Price}$)</h4>
-            <span className="text-primary d-block mt-3">Description :</span>
-            <p className="card-text">{Description}</p>
-          </dir>
-          <div className="container d-flex flex-wrap align-content-center">
-            {MorePhoto&&(MorePhoto.map((pic) => {
+          <div class="col-12 d-flex justify-content-around pucadm pt-4">
+          {MorePhoto&&(MorePhoto.map((pic) => {
               return (
-                <img
-                  className="card-img-top h-75 w-25  border border-primary rounded rounded-5 me-2"
-                  src={pic}
-                  alt={pic.id}
-                  onClick={changeImg}
-                />
-              );
-            }))}
+                      <img
+                        src={pic}
+                        alt={Title}
+                        onClick={changeImg}
+                      />
+                    );
+                  }))}
           </div>
-        </div>
-        <Link
-          to="/Cart"
-          onClick={() => dispatch(AddTocart(Id))}
-          className="btn btn-primary"
-        >
-          Add to cart
-        </Link>
+      </div>
+      <div class="col-lg-6 col-sm-12 col-md-4 pt-3 ps-5">
+          <h3 class="fw-bold pb-4 pt-3">{Title}</h3>
+          <hr/>
+          <span class = "product-price">{Price}</span>
+          <span class = "old-price text-dark ">44000</span>
+          <div class="pt-3 ps-1 row">
+            <p class="col-12">QUANTITÉ</p>
+            <div class="quantité col-5 pt-1">
+              <a class="pe-5" href="">-</a>2<a class="ps-5" href="">+</a>
+              <hr class="quantité"/>
+            </div>
+            <div class="col-5 ps-5 adto" >
+                  <Link
+                    to="/Cart"
+                    onClick={() => dispatch(AddTocart(Id))}
+                    className="btn btn-primary"
+                  >
+                    Add to cart
+                  </Link>  
+              </div>
+          </div>
       </div>
     </div>
+  </main>
   ) :  <>Loading</>
 
 };
