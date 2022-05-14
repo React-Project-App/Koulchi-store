532244f2ee225f78a25a1ff6a833dcf6e160afa2
walid <walid@gmail.com>
2022-05-15 00:34:29 +0200

    Styling cart and Product details

--- src/Components/CartItems/CartItems.js
+++ src/Components/CartItems/CartItems.js
@@ -3,7 +3,7 @@ import { useDispatch } from 'react-redux';
 import { DicresAmount, IncresAmount, RemoveFromCart } from '../../Actions/Cart';
 
 const CartItems = ({item}) => {
- const {Price,Amount,SubTotal,Title,id} =item
+ const {Price,Amount,SubTotal,Title,id,Photo} =item
  
  const dispatch=useDispatch() ;
 
@@ -21,28 +21,43 @@ const  HandleDicresAmount=()=>{
   
     return (
 
-      <div   className='shadow'>
-        
-        <p> {Amount}</p>
-        <p>{SubTotal}</p>
-        <p>{Title}</p>
-        <button 
-        onClick={HandleRemoveFromCart}
-        >
-          remove
-        </button>
-        <button 
-        onClick={HandleIncresAmount} 
-        className=" ms-5 btn btn-outline-success"
-        >+
-        </button>
-        <button 
-        onClick={HandleDicresAmount}  
-        className="ms-2 btn btn-outline-danger"
-        >-
-        </button>
-        
+  <div class="Cart-Items my-4 row">
+      <div class="image-box col-md-4 col-sm-12 d-flex justify-content-center">
+      <img src={Photo} className="image-fluid rounded-4" style={{ height:"120px" }} />
       </div>
+      <div class="about col-md-4 col-sm-12 d-flex justify-content-center">
+      <h1 class="title ">{Title}</h1>
+      {/* <h3 class="subtitle">250ml</h3> */}
+      </div>
+      <div class="counter col-md-4 d-flex justify-content-center col-sm-12">
+        <div class="btn " onClick={HandleIncresAmount} >+</div>
+        <div class="count px-4">{Amount}</div>
+        <div class="btn" onClick={HandleDicresAmount}>-</div>
+      </div>
+      <div class="prices">
+        <div class="amount">{SubTotal} DH</div>
+        <div class="remove" onClick={HandleRemoveFromCart}><u>Remove</u></div>
+      </div>
+ </div>
+        // {/* <p> </p>
+        // <p>{SubTotal}</p>
+        // <p></p> */}
+        // {/* <button 
+        // 
+        // >
+        //   remove
+        // </button>
+        // <button 
+        // onClick={HandleIncresAmount} 
+        // className=" ms-5 btn btn-outline-success"
+        // >+
+        // </button>
+        // <button 
+        // onClick={HandleDicresAmount}  
+        // className="ms-2 btn btn-outline-danger"
+        // >-
+        // </button> */}
+        
     )
 
   
--- src/Pages/ProductDetails/ProductDetails.jsx
+++ src/Pages/ProductDetails/ProductDetails.jsx
@@ -2,13 +2,15 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { AMOUNTPRODUCT, GETDETAILSPRODUCT } from "../../Actions/Poroducts";
-
+import {BsStarFill ,BsHeart,BsHeartFill} from 'react-icons/bs'
 import { Link } from "react-router-dom";
 import { AddTocart } from "../../Actions/Cart";
 import "../../CssFiles/style.css";
 const ProductDetails = () => {
   const { Id } = useParams();
   const [pic, setpic] = useState("");
+  const [heart,setheart]=useState(false);
+
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(GETDETAILSPRODUCT(Id));
@@ -35,47 +37,69 @@ const [Amount, setAmount] = useState(1);
     setpic(e.currentTarget.src);
   }
   return Title ? (
-    <main className="container">
-      <div className="row pt-5">
-        <div className="col-lg-5 col-sm-12 col-md-8 row pt-3">
+    <main className="container mt-5 pt-4">
+      <div className="row pt-5 ps-4">
+        <div className="col-lg-5 col-sm-12 col-md-6 row pt-4">
           <div className="col-12 big w-auto">
             <img src={pic ? pic : Photo} className="img-fluid" alt={Title} />
           </div>
           
-          <div className="col-12 d-flex justify-content-around pucadm pt-4">
+          <div className="col-12 d-flex justify-content-around pucadm py-4 ">
             {MorePhoto &&
               MorePhoto.map((pic) => {
-                return <img src={pic} alt={Title} onClick={changeImg} />;
+                return <img src={pic} alt={Title} className="col-3 mt-4" onClick={changeImg} />;
               })}
           </div>
         </div>
-        <div className="col-lg-6 col-sm-12 col-md-4 pt-3 ps-5">
+        <div className="col-lg-7 col-sm-12 col-md-6 pt-3 ps-lg-5">
           <h3 className="fw-bold pb-4 pt-3">{Title}</h3>
           <hr />
           <span className="product-price">{Price}</span>
           <span className="old-price text-dark ">44000</span>
           <div className="pt-3 ps-1 row">
-            <p className="col-12">QUANTITÉ</p>
-            <div className="quantité col-5 pt-2  row">
-              <a className=" col-4 text-center" onClick={handelDecAmount}>
-                -
-              </a>
-              <p className="col-4 text-center">{Amount}</p>
-              
-              <a className="col-4 text-center"  onClick={handelInAmount}>
-                +
-              </a>
-              <hr className=" ms-4 col-8" />
+           
+            <div className="pt-2 ps-1 row col-12 d-flex justify-content-around">
+            <div className="quantité col-5  row">
+            <p className="fw-bold ms-4 ">QUANTITÉ</p>
+            <a className=" col-4 text-center fw-bold pointer Amount" onClick={handelDecAmount}>-</a>
+            <p className="col-4 text-center mt-2 fw-bold">{Amount}</p>
+            <a className="col-4 text-center fw-bold pointer Amount" onClick={handelInAmount}>+</a>
+            <hr className="ms-4 col-9 "/>
+          </div>
             </div>
-            <div className="col-5 ps-5 adto">
+            <div className="ps-4 pt-1 adto d-flex col-7 row ">
               <Link
                 to="/Cart"
                 onClick={() => dispatch(AddTocart(Id,Amount))}
-                className="btn btn-primary"
+                className="col-9 btn1"
               >
                 Add to cart
               </Link>
+              { (heart  ?  
+                <BsHeartFill className='heart-fill pointer col-3 heart' onClick={_=>setheart(!heart)}/> :
+                <BsHeart className='heart-fill pointer col-3  heart' onClick={_=>setheart(!heart)}/>)}
+              
             </div>
+          </div>
+          <div className="ps-1 pt-3">
+          <p className="fw-bold pb-1 ps-1">PRODUCT DETAILS</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vestibulum orci a sapien imperdiet maximus.
+            Suspendisse vel ex eget mauris efficitur finibus eu a enim. Aenean tellus neque, consectetur sit amet lacus
+            a,
+            hendrerit ornare urna. Fusce venenatis lorem vitae augue suscipit, eu tempor massa vulputate. Praesent
+            iaculis velit libero,
+            vel faucibus sem pulvinar vel. Curabitur viverra nisl dui, a venenatis felis pellentesque id. Donec non
+            lacus diam.
+          </p>
+          <div className="d-flex justify-content-start review mt-4 align-items-center">
+           <BsStarFill/>
+           <BsStarFill/>
+           <BsStarFill/>
+           <BsStarFill/>
+           <BsStarFill/>
+            <span className="ps-2">(review 150)</span>
+          </div>
           </div>
         </div>
       </div>
